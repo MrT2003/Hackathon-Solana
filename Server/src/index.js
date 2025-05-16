55474f5e2a2ae76f890a86f9bcdc1f2fc5438390
Hoanghuygia <hoanghuy051230@gmail.com>
2025-05-16 07:46:16 +0700

    config change

--- Server/src/index.js
+++ Server/src/index.js
@@ -3,9 +3,8 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors'
 import {route} from './routes/index.js'
-import { handleError } from './helper/handleError.js';
-import dotenv from 'dotenv';
-dotenv.config();
+import { handleError } from './middlewares/index.js';
+import config from './config/index.js';
 
 const app = express();
 
@@ -22,7 +21,7 @@ app.use(cors());
 route(app)
 handleError(app)
 
-const PORT = process.env.PORT || 5000;
+const PORT = config.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
