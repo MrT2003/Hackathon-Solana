44b8c5cb294e5124e429bc5808ecabd9653f7824
PhungNghiaMa <phungnghia531@gmail.com>
2025-05-24 23:42:08 +0700

    update signup function

--- Server/src/app/controllers/UserController.js
+++ Server/src/app/controllers/UserController.js
@@ -14,16 +14,17 @@ export class UserController {
 
     signup = async (req, res) => {
         try {
-            const user = req.body;
-            if (req.user && req.user.uid !== undefined){
-                return res.status(200).json({ message: "User is already signed up !" });
-            }
-            res.status(202).json({ message: "User is authenticated" , success: true } );
-            const result = await registerNewUser(user);
-            if (result.success) {
-                console.info("User registered successfully !");
-            }else{
-                console.error("User registered failed !");
+            res.status(200).json({ success: true, message: "User has signed up !" });
+            if (req.user && req.user.uid) {
+                const user = req.body;
+                const result = await registerNewUser(user);
+                if (result.success) {
+                    console.info("User registered successfully !");
+                    return res.status(201).json({ success: true, message: "User registered successfully" });
+                } else {
+                    console.error("User registration failed !");
+                    return res.status(400).json({ success: false, message: result.message });
+                }
             }
         } catch (error) {
             console.error("Error in register:", error);
